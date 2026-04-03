66fd07ad6be87816be3af69bff09c2afb7a84e89
xuz1 <xuz@niehs.nih.gov>
2026-04-03 12:58:09 -0400

    bugfix
    Signed-off-by: xuz1 <xuz@niehs.nih.gov>

---
+++ R/zzz.R
@@ -0,0 +1,27 @@
+#define a few global variables
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c(
+    "DNAmFitnessModels",
+    "EpiToc_CpGs",
+    "GrimAgeV1_model",
+    "GrimAgeV2_model",
+    "GrimAge_CpG",
+    "MiAgedat",
+    "bAge_CpG",
+    "cAge_CpG",
+    "episcore_model",
+    "libraryDataGet",
+    "mPOA_Models",
+    "methscore_dict",
+    "pcc1",
+    "pcc2",
+    "pcc3",
+    "pcc4",
+    "refmeth",
+    "refmeth2",
+    "AdaptAge_model", 
+    "CausAge_model", 
+    "DamAge_model"
+  ))
+}
+
